2c255eb85b0e492024948643746e3c74481e1663
odeclasonarres <odeclasonarres@gmail.com>
2019-03-11 13:49:30 +0100

    Subiendo la estructura

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lecturas
+
+Proyecto Django para una app de gestión y seguimiento de lecturas
+
+Puedes verlo en funcionamiento [aquí](). (Próximamente)
--- lecturas/models.py
+++ lecturas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
+class Lectura(models.Model):
+    titulo=models.CharField(max_length=200)
+    autor=models.CharField(max_length=200)
+    valoracion=models.IntegerField
+    categoria=models.CharField(max_length=200)
+    tema=models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    lectura_date = created_date = models.DateTimeField(blank=True, null=True))
 # Create your models here.
